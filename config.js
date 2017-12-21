9db0319a052a706a5054bc32504f877126449893
Ptonmi2016 <ocwcheng@gmail.com>
2017-12-21 10:02:20 +0100

    twitter API key

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  consumer_key:         'LgPPnNQsxYMneFfU8ampRMgVT',
-  consumer_secret:      'MNlGCEqMCL2tUlEq0C6iQAadUbjAJucNuSHZGPiAf1pfwwIBix',
-  access_token:         '3363796089-aWjtwpi9ZmIlIAqD3zPLrytbxb2PbJ7Jq57Xc3i',
-  access_token_secret:  'Wyx7lXijdn9rqwN6Sj5jBu6RkzR2KGfZ5AnijxLYQnAwq',
+  consumer_key:         'key',
+  consumer_secret:      'key',
+  access_token:         'key',
+  access_token_secret:  'key',
 }
+//register the API key of twitter 
